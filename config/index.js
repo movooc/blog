c0fa9d687a17a229183060441df501320ea61b60
movooc <81326152@qq.com>
2017-04-21 11:33:42 +0800

    some update webpack config

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: '',
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '',
+    assetsPublicPath: 'http://assets.sandbox.yike.fm/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
