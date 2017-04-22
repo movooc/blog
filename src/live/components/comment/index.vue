374a585262eeb3cd67f47ef4ffe6f31ddaca2596
movooc <81326152@qq.com>
2017-04-22 16:49:15 +0800

    update live comment module

--- src/live/components/comment/index.vue
+++ src/live/components/comment/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="l-comment" v-if=commentShow>
+  <div class="l-comment" v-if='isOwner || commentShow'>
     <div class="back-live" v-if="!isPC"><button @click="hideComment">回到直播</button></div>
     <div class="comment-body">
       <div class="comment-title">讨论区</div>
@@ -50,7 +50,7 @@
     },
     computed: {
       ...mapState([
-        'commentShow',
+        'commentShow', 'isOwner',
       ])
     },
     methods: {
