5dda224e939bf1ca148410e70965aebc990a25e4
movooc <81326152@qq.com>
2017-04-28 19:04:17 +0800

    some update

--- src/student/components/courselist.vue
+++ src/student/components/courselist.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="course-list">
-    <ul>
+    <ul class="lesson">
       <li v-for="list in lists">
-        <a href="javascript:;" @click="startLesson(list.sn)">点击开始{{list.title}}</a>
+        <a href="javascript:;" @click="startLesson(list.sn)">
+          <div class="list-img">
+            <img :src="list.cover" alt="">
+          </div>
+          <div class="list-content">
+            <div class="tit">
+              {{list.title}}
+            </div>
+            <p class="date">
+              {{list.tms_update}}
+            </p>
+          </div>
+        </a>
         <!--<router-link :to="{ name: 'detail', params: { lesson_sn: list.sn }}">点击开始{{list.title}}</router-link>-->
       </li>
     </ul>
@@ -50,5 +62,54 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import '~@lib/css/index.styl';
 
+  .course-list{
+    .lesson {
+      padding: 29px;
+      background: #fff;
+      list-style-type: none;
+
+      a{
+        text-decoration: none;
+      }
+      li+li{
+        border-top:1px solid #E6EAF2;
+      }
+      li{
+        position: relative;
+        padding-top: 29px;
+        height: 182px;
+        overflow: hidden;
+
+        a{
+          display: block;
+          height: 100%;
+        }
+        .list-img{
+          float: left;
+          width: 281px;
+
+          img{
+            width: 100%;
+          }
+        }
+
+        .list-content{
+          float: left;
+          padding-left: 20px;
+          .tit{
+            color: #444;
+          }
+          .date{
+            margin: 0;
+            padding-top: 13px;
+            color: #AAA;
+            px2px(font-size, 26px);
+          }
+        }
+      }
+    }
+
+  }
 </style>
--- src/teacher/components/courselist.vue
+++ src/teacher/components/courselist.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="course-list">
     <ul>
+      <li class="head">
+        <span>创建时间</span>
+        <span>标题</span>
+        <span class="handle">操作</span>
+      </li>
       <li v-for="list in lists">
-        <a href="javascript:;" @click="startLesson(list.sn)">点击开始{{list.title}}</a>
-        <!--<router-link :to="{ name: 'detail', params: { lesson_sn: list.sn }}">点击开始{{list.title}}</router-link>-->
+        <span>{{list.tms_update}}</span>
+        <span>{{list.title}}</span>
+        <div class="handle">
+          <a href="javascript:;" @click="startLesson(list.sn)">进入课程</a>
+        </div>
       </li>
     </ul>
   </div>
@@ -50,5 +58,37 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  .course-list{
+    margin: 0 auto;
+    width: 980px;
 
+    li {
+      padding: 0 5px;
+      display: -webkit-box;
+      display: box;
+      border-bottom: 1px solid #E3E3E3;
+      line-height: 26px;
+
+      &.head {
+        background: #E3E3E3;
+      }
+      >*{
+        display: -webkit-box;
+        display: box;
+        -webkit-box-flex: 1;
+        box-flex: 1;
+        width: 200px;
+        justify-content: center;
+        border-right: 1px solid #E3E3E3;
+
+        &:last-child{
+          border: 0 none;
+        }
+        &.handle{
+          -webkit-box-flex: 3;
+          box-flex: 3;
+        }
+      }
+    }
+  }
 </style>
