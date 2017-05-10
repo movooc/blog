5602387538b21fba4197b1170c5c75e9bc496955
movooc <81326152@qq.com>
2017-05-10 18:09:52 +0800

    update live modules

---
+++ src/lib/js/setWechatTitle.js
@@ -0,0 +1,20 @@
+const SetWechatTitle = function(title) {
+  document.title = title;
+  const mobile = navigator.userAgent.toLowerCase();
+  if (/iphone|ipad|ipod|Android/.test(mobile)) {
+    let iframe = document.createElement('iframe');
+    iframe.style.display = 'none';
+    // 替换成站标favicon路径或者任意存在的较小的图片即可
+    //iframe.setAttribute('src', '/favicon.ico');
+    let iframeCallback = function () {
+      setTimeout(function () {
+        iframe.removeEventListener('load', iframeCallback);
+        document.body.removeChild(iframe)
+      }, 0)
+    };
+    iframe.addEventListener('load', iframeCallback);
+    document.body.appendChild(iframe);
+  }
+};
+
+export default SetWechatTitle;
