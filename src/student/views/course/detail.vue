963149104c4a5bbf7af9b587cd530a76bab7761b
movooc <81326152@qq.com>
2017-06-09 18:28:07 +0800

    some update

--- src/student/views/course/detail.vue
+++ src/student/views/course/detail.vue
@@ -78,11 +78,12 @@
         // 微信操作
         try{
           wx.ready(() => {
+            let shareLink = `${window.location.href}&origin=share&share_url=${encodeURIComponent('?#/'+window.location.href.split('#')[1])}`;
             // 微信发送给朋友
             wx.onMenuShareAppMessage({
               title: data.title, // 分享标题
               desc: data.brief, // 分享描述
-              link: `${window.location.href}&origin=share`, // 分享链接
+              link: shareLink, // 分享链接
               imgUrl: this.courseDetail.cover, // 分享图标
               type: '', // 分享类型,music、video或link，不填默认为link
               dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
@@ -98,7 +99,7 @@
             // 分享到朋友圈
             wx.onMenuShareTimeline({
               title: data.title, // 分享标题
-              link: `${window.location.href}&origin=share`, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+              link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
               imgUrl: this.courseDetail.cover, // 分享图标
               success: () => {
                 // 用户确认分享后执行的回调函数
