2f2aea3c57e6fff777506b05ff6f108c1d575ab1
movooc <81326152@qq.com>
2017-06-16 13:53:23 +0800

    add tools

--- src/lib/js/mUtils.js
+++ src/lib/js/mUtils.js
@@ -395,3 +395,18 @@ export const convertToArray = (arr, data) => {
     return [...arr, data];
   }
 };
+
+export const strlen = ( str ) => {
+  var len = 0;
+  for (var i=0; i<str.length; i++) {
+    var c = str.charCodeAt(i);
+    //单字节加1
+    if ((c >= 0x0001 && c <= 0x007e) || (0xff60<=c && c<=0xff9f)) {
+      len++;
+    }
+    else {
+      len+=2;
+    }
+  }
+  return len;
+};
