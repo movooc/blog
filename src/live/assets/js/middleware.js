f74232ac550957cebb5fa1642f446d4afda18454
movooc <81326152@qq.com>
2017-06-22 17:11:59 +0800

    update

--- src/live/assets/js/middleware.js
+++ src/live/assets/js/middleware.js
@@ -4,8 +4,6 @@ import { getCookie } from '@lib/js/mUtils';
 export const Vueinterceptors = function () {
   Vue.http.interceptors.push(function(request, next) {
     // add
-    console.log('vue.http.headers');
-    console.log(Vue.http.headers);
     Vue.http.headers.common['X-SESS'] = getCookie('sess');
 
     next(function (response) {
